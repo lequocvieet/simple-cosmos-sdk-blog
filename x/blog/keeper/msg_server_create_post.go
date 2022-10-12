6acc49b958db42f957cbd5a89ef7c179a23022b5
vieet <lequocvieet>
2022-10-12 14:34:20 +0700

    ignite init project

---
+++ x/blog/keeper/msg_server_create_post.go
@@ -0,0 +1,26 @@
+package keeper
+
+import (
+	"context"
+
+	"blog/x/blog/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (k msgServer) CreatePost(goCtx context.Context, msg *types.MsgCreatePost) (*types.MsgCreatePostResponse, error) {
+	//get the context
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// Create variable of type Post
+	var post = types.Post{
+		Creator: msg.Creator,
+		Title:   msg.Title,
+		Body:    msg.Body,
+	}
+
+	//Add a post to the store and get back ID
+	id := k.AppendPost(ctx, post)
+
+	return &types.MsgCreatePostResponse{ID: id}, nil
+}
